dcc436198a33e7fc902537abb3221b632e8f7b72
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2023-06-22 09:37:23 -0300

    121 - CRUD REST Basics

--- app.py
+++ app.py
@@ -4,11 +4,36 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello':'world'}
+puppies = []
 
-api.add_resource(HelloWorld, '/')
+class PuppyNames(Resource):
+    def get(self, name):
+        for pup in puppies:
+            if pup['name'] == name:
+                return pup
+        return {'name': None}, 404
+
+    def post(self, name):
+        pup = {'name' : name}
+        puppies.append(pup)
+        return pup
+
+    def delete(self, name):
+        for pup in puppies:
+            if pup['name'] == name:
+                deleted_pup = pup
+                puppies.remove(pup)
+                return {
+                    'note': 'delete successful',
+                    'name': deleted_pup['name']
+                    }
+
+class AllNames(Resource):
+    def get(self):
+        return {'puppies': puppies}
+
+api.add_resource(PuppyNames, f'/puppy/<string:name>')
+api.add_resource(AllNames, f'/puppies')
 
 if __name__=='__main__':
     app.run(debug=True)
